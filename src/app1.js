845351bf3ccd1ddd7b285bb429768f479bef6ccf
Schnarfengargle123 <schnarfengargle123@gmail.com>
2021-07-24 15:01:30 +0100

    Made changes

--- src/app1.js
+++ src/app1.js
@@ -1,12 +1,25 @@
 const button = document.getElementById("change-color");
 const title = document.getElementById("title");
+const text = document.getElementById("text");
+const link = document.getElementById("link")
 
 function changeColor() {
     button.style.color = ('darkseagreen');
     button.style.backgroundColor = ('deeppink');
+    button.innerHTML = ("Colour Changed!");
 
     title.style.color = ('purple');
-    title.style.backgroundColor = ('lime');
+    title.style.backgroundcolor = ('lime');
+    title.innerHTML = ("i am a title");
+
+    text.style.color = ('amber');
+    text.style.backgroundcolor = ('cyan');
+    text.style.fontSize = ("300px");
+    text.innerHTML = ("i am a text");
+
+    link.style.color = ('tomato');
+    link.style.backgroundColor = ('crimson');
+    link.innerHTML = ("you can't see me");
 }
 
 button.onclick = changeColor;
